e3ceaba1f0e9a6463d9fbfe2b0e543ef32d6cd00
Leon Ackermann <leonackermann@Leons-MBP.fritz.box>
2022-11-07 15:00:56 +0100

    some changes

--- 2 Multi-Layer-Perceptron/Leon drafts/layer.py
+++ 2 Multi-Layer-Perceptron/Leon drafts/layer.py
@@ -1,24 +1,36 @@
 # still everything to import
+import numpy as np
 
 class Layer:
     
-    def __init__(self, n_units: int, input_units: int):
+    def __init__(self, n_units: int, input_units: int, layer_input, layer_activation):
         self.n_units = n_units # number of units in layer
         self.input_units = input_units # number of inputs from previous layer
         self.weights = None # initiate matrix shape(n inputs, n units) random values for the weights and zeros for the biases.
-        self.layer_input = None # array with inputs from previous layer
-        self.layer_preactivation = None
-        self.layer_activation = None
+        self.layer_input = None # array with outputs from previous layer
+        self.layer_preactivation = None # array with all inputs for ReLu function
+        self.layer_activation = layer_activation # array with all activations of next layer
+
+    def ReLU(z):
+        return np.maximum(0,z)
 
     def forward_step(self):
         output = None # generate numpy array with all zeros
-        for i in range(len(self.n_units)):
-            temp = self.layer_input@self.weights[i] # matrix multiplication with input and weights of i-th unit
-            if temp > 0:
-                output[i] = 1
+        z = self.weights.dot()
+
+        return output
 
     def backward_step(self):
-        pass
+        # how much the weights influence the outcome
+        gradients_weights = None # self.layer_input * derivative of relu(self.activations) * derivitave of activation of next layer
+        
+        # how much the bias influences the outcome
+        gradients_bias = None # derivative of relu(self.activations) * derivitave of activation of next layer
+        
+        # how much the input influences the outcome
+        gradients_input = None # for k in len(self.layer_input) sum(self.weights[k]* derivative of relu(self.activations)*derivative of activation of next layer
+
+        self.weights = None # update the parameters in right direction of the gradients
 
 
 
